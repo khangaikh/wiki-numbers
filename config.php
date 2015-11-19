4a8ef08976a798042afabbfd33e704e9c1a0c6ad
khangaikh <khangai@alchemist.mn>
2015-11-19 15:45:47 +0800

    Template loaded

--- config.php
+++ config.php
@@ -3,9 +3,9 @@
 	require_once 'includes/parse/autoload.php';
 	use Parse\ParseClient;
     
-    $app_id = 'GBKoGmSywZrOJPdHzpZnFzfKqoPZ5nOvpfhnseIr';
-    $rest_key = 'UTtaIElx7BCxftei5meS1GB8uk1T3wrKSrmEg21K';
-    $master_key = 'wr6tBzUnsPEjgowX0LHaYA20ggme2KsJZH8AYjuk';
+    $app_id = '5HNUInP58m0Ofu92MDK8sMGdQZqj3Rwj6U1QFL2t';
+    $rest_key = 'EIenrwXw1fx1h2kD4trd9CRxsN0k5otWvh6OFHiU';
+    $master_key = 'oRLc9urA5uMIFKvWK1r3Ut703zhGmegNXp0nbQBn';
     ParseClient::initialize( $app_id, $rest_key, $master_key );
 
 ?>
